104d6a2a05ea05d0845632c114c8c4a2f6427dd1
Alexander Skjolden <alex@plastboks.net>
2017-03-24 10:11:47 +0100

    HasMyVote for eventImage

--- src/main/java/io/hobaskos/event/service/mapper/EventImageMapper.java
+++ src/main/java/io/hobaskos/event/service/mapper/EventImageMapper.java
@@ -1,38 +1,57 @@
 package io.hobaskos.event.service.mapper;
 
 import io.hobaskos.event.domain.*;
+import io.hobaskos.event.repository.EventImageVoteRepository;
+import io.hobaskos.event.repository.EventPollRepository;
+import io.hobaskos.event.service.UserService;
 import io.hobaskos.event.service.dto.EventImageDTO;
 
 import org.mapstruct.*;
+
+import javax.inject.Inject;
 import java.util.List;
 
 /**
  * Mapper for the entity EventImage and its DTO EventImageDTO.
  */
 @Mapper(componentModel = "spring", uses = {})
-public interface EventImageMapper {
+public abstract class EventImageMapper {
+
+    @Inject
+    private UserService userService;
+
+    @Inject
+    private EventImageVoteRepository eventImageVoteRepository;
 
     @Mapping(target = "file", ignore = true)
     @Mapping(target = "fileContentType", ignore = true)
     @Mapping(source = "poll.id", target = "pollId")
     @Mapping(source = "user.login", target = "userLogin")
-    EventImageDTO eventImageToEventImageDTO(EventImage eventImage);
+    public abstract EventImageDTO eventImageToEventImageDTO(EventImage eventImage);
 
-    List<EventImageDTO> eventImagesToEventImageDTOs(List<EventImage> eventImages);
+    public abstract List<EventImageDTO> eventImagesToEventImageDTOs(List<EventImage> eventImages);
 
     @Mapping(source = "pollId", target = "poll")
     @Mapping(target = "votes", ignore = true)
     @Mapping(target = "user", ignore = true)
-    EventImage eventImageDTOToEventImage(EventImageDTO eventImageDTO);
+    public abstract EventImage eventImageDTOToEventImage(EventImageDTO eventImageDTO);
 
-    List<EventImage> eventImageDTOsToEventImages(List<EventImageDTO> eventImageDTOs);
+    public abstract List<EventImage> eventImageDTOsToEventImages(List<EventImageDTO> eventImageDTOs);
 
-    default EventPoll eventPollFromId(Long id) {
+    public EventPoll eventPollFromId(Long id) {
         if (id == null) {
             return null;
         }
         EventPoll eventPoll = new EventPoll();
         eventPoll.setId(id);
         return eventPoll;
+    }
+
+    @AfterMapping
+    protected void sideLoadMetaData(EventImage eventImage, @MappingTarget EventImageDTO result) {
+        User user = userService.getUserWithAuthorities();
+        if (user == null) return;
+        eventImageVoteRepository.findFirstByEventImageAndUser(eventImage, user)
+            .ifPresent(eventImageVote -> result.setHasMyVote(true));
     }
 }
